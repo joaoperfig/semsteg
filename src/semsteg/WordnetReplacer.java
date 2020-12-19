e4341efac0f4dad2782ed5d9dc9bbe60ac64af20
joaoperfig <joaoperfig@gmail.com>
2020-12-19 03:53:09 +0000

    refactor replacer

--- src/semsteg/WordnetReplacer.java
+++ src/semsteg/WordnetReplacer.java
@@ -58,70 +58,15 @@ public class WordnetReplacer implements Replacer {
 
 	@Override
 	public List<List<TextPart>> sectionReplacements (List<TextPart> section) {
-		List<List<TextPart>> wordReplacements = new ArrayList<List<TextPart>>();
-		List<Integer> numReplacements = new ArrayList<Integer>();
-		int totalReplacements = 1;
-		for (TextPart part : section) {
-			if (part instanceof Word) {
-				List<TextPart> theseReplacements = new ArrayList<TextPart> ();
-				theseReplacements.addAll(replacements((Word) part));
-				wordReplacements.add(theseReplacements);
-				numReplacements.add(theseReplacements.size());
-				totalReplacements *= theseReplacements.size();
-				
-			} else {
-				List<TextPart> theseReplacements = new ArrayList<TextPart> ();
-				theseReplacements.add(part);
-				wordReplacements.add(theseReplacements);
-				numReplacements.add(theseReplacements.size());
-			}
-		}
-		
-		//System.out.print(numReplacements);
-		//System.out.println(totalReplacements);
-		
-		List<Integer> current = new ArrayList<Integer>();
-		for(int i = 0; i<numReplacements.size(); i++) {
-			current.add(0);
-		}
-		List<Integer> last;
-		List<List<Integer>> replacementSeqs = new ArrayList<List<Integer>>();
-		
-		replacementSeqs.add(current);
-		
-		for (int n=0; n<totalReplacements-1; n++) {
-			last = current;
-			current = new ArrayList<Integer>();
-			for (int i=0; i<numReplacements.size(); i++) {
-				current.add(last.get(i));
-			}
-			
-			for (int i=0; i<numReplacements.size(); i++) {
-				if (current.get(i) == numReplacements.get(i)-1) {
-					current.set(i, 0);
-				} else {
-					current.set(i, current.get(i)+1);
-					break;
-				}
-			}
-			replacementSeqs.add(current);
-		}
-		
 		List<List<TextPart>> result = new ArrayList<List<TextPart>>();
-		List<TextPart> curres;
 		
-		for (int n=0; n<totalReplacements; n++) {
-			//System.out.print("    ");
-			//System.out.println(replacementSeqs.get(n));
-			curres = new ArrayList<TextPart>();
-			for (int i=0; i<wordReplacements.size(); i++) {
-				curres.add(wordReplacements.get(i).get(replacementSeqs.get(n).get(i)));
-			}
-			result.add(curres);
-			
-			//System.out.println(curres);
+		setSectionReplace(section);
+		
+		List<TextPart> current = getNext();
+		while (current != null) {
+			result.add(current);
+			current = getNext();
 		}
-		//System.out.println("");
 		
 		return result;
 	}
