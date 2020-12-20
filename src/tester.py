8620fd96afc6acaf17b4fba44fe7dab5cee18e9a
joaoperfig <joaoperfig@gmail.com>
2020-12-20 03:14:57 +0000

    run tests

--- src/tester.py
+++ src/tester.py
@@ -2,10 +2,10 @@ import os
 import glob
 
 def main():
-    directory = "../data/globwbe/"
-    outdir = "../data/globwbe_stats/"
-    #directory = "../data/cohacorpus/"
-    #outdir = "../data/cohacorpus_stats/"    
+    #directory = "../data/globwbe/"
+    #outdir = "../data/globwbe_stats/"
+    directory = "../data/cohacorpus/"
+    outdir = "../data/cohacorpus_stats/"    
     owd = os.getcwd()
     os.chdir(directory)
     files = glob.glob("*.txt")
